dfc2ba302bef3b31e142823bf7bc9485315a4f4a
silicon-valley <64067534+aaron-silicon-valley@users.noreply.github.com>
2021-03-13 20:38:38 -0600

    v1.0

---
+++ madlib.py
@@ -0,0 +1,38 @@
+holiday = 'holiday'
+noun1 = 'wedding'
+place = 'france'
+person = 'hulk'
+adj1 = 'big'
+body_part = 'arm'
+verb1 = 'run'
+adj2 = 'hard'
+noun2 = 'investment'
+food = 'pizza'
+plural_noun = 'saving'
+
+
+madlibs = "I can't believe it's already {holiday}! \n" \
+          "I can't wait to put on my {noun1} and visit every {place} in my neighborhood.\n" \
+          "This year, I am going to dress up as {person} with {adj1} {body_part}.\n" \
+          "Before I {verb1}, I make sure to grab my {adj2} {noun2} to hold all of my {food}.\n" \
+          "Finally, all of my {plural_noun} are ready to go!".format(holiday = 'holiday',
+noun1 = 'wedding',
+place = 'france',
+person = 'hulk',
+adj1 = 'big',
+body_part = 'arm',
+verb1 = 'run',
+adj2 = 'hard',
+noun2 = 'investment',
+food = 'pizza',
+plural_noun = 'saving')
+# holiday, noun1, place, person, adj1, body_part, verb1, adj2, noun2, food, plural_noun
+
+print(madlibs)
+
+# Git init
+# Git add .
+# Git status
+# Git commit -m “comment”
+# Git remote add origin url
+# Git push -u origin master
